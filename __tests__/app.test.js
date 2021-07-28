b81277481ce455a09dff4c90f3b92b365eaedaa1
BenFrancka <benfrancka@gmail.com>
2021-07-28 16:06:42 -0700

    adds initial test for POST route with twilio message to app.test.js

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,9 +2,34 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import Item from '../lib/models/Inventory.js';
 
-describe('demo routes', () => {
+jest.mock('twilio', () => () => ({
+  messages: {
+    create: jest.fn(),
+  },
+}));
+
+describe('inventory routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+  it('creates an inventory item using POST and sends a text message', async () => {
+    const potatoes = {
+      itemName: 'potatoes 50#',
+      itemCategory: 'produce',
+      price: 7,
+      inStock: true
+    };
+    const res = request(app)
+      .post('api/v1/inventory')
+      .send(potatoes);
+    
+    expect(createMessage).toHaveBeenCalledTimes(1);
+    expect(res.body).ToEqual({
+      id: '1',
+      ...potatoes
+    });
+  });
+
 });
